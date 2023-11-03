fabc275a9608e76755de6d437ec8b63e4e48ce1c
Akrem Boughazi <kikoua203@gmail.com>
2023-11-03 16:36:53 +0100

    fixing the api url bug

--- app/utils/connections.js
+++ app/utils/connections.js
@@ -1,1 +1,3 @@
-export const BASE_URL = process.env.API || 'http://localhost:3000/'
+// export const BASE_URL = process.env.API || 'https://codeeaseblog.vercel.app/'
+// export const BASE_URL = process.env.API || 'http://localhost:3000/'
+export const BASE_URL = 'https://codeeaseblog.vercel.app/'
